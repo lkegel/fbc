6f9d9aaf7a268438d00a2e21af787a1889f7cd97
Lars Kegel <lars.kegel@tu-dresden.de>
2019-11-21 10:51:45 +0100

    Update config files

--- Implementation/configs/config.R
+++ Implementation/configs/config.R
@@ -7,10 +7,13 @@ config_read_csv <- function(config_name, config_parse) {
   stopifnot(file.exists(fp))
   
   # tbl <- read.table(fp, T, ";", check.names = F, stringsAsFactors = F)
-  tbl <- read.xlsx(fp, sheet = 1, skipEmptyRows = FALSE)
+  wb <- openxlsx::loadWorkbook(fp)
   res <- list()
-  for (irow in seq(nrow(tbl))) {
-    res[[irow]] <- config_parse(tbl[irow, ])
+  for (wb_name in names(wb)) {
+    tbl <- read.xlsx(fp, sheet = wb_name, skipEmptyRows = FALSE)  
+    for (irow in seq(nrow(tbl))) {
+      res <- c(res, list(as.list(tbl[irow, ])))
+    }
   }
   names(res) <- as.character(unlist(lapply(res, function(x) x[[1]])))
   
@@ -29,15 +32,13 @@ a_parse <- function(row) {
 
 d <- config_read_csv("d", a_parse)
 
-m <- config_read_csv("fbg", a_parse)
-m <- c(m, config_read_csv("tsfresh", a_parse))
-m <- c(m, config_read_csv("dwt", a_parse))
-m <- c(m, config_read_csv("rld", a_parse))
+m <- config_read_csv("m", a_parse)
 
 s <- config_read_csv("s", a_parse)
 
 f <- config_read_csv("f", a_parse)
 
+c <- config_read_csv("c", a_parse)
 # rt <- config_read_csv("rt", a_parse)
 # 
 # d <- config_read_csv("d", a_parse)
